0b9103131103dd55b9323d61808181d94b0894b0
Juan Ignacio Yarbuh <jyarbuh@est.frba.utn.edu.ar>
2020-07-22 12:36:15 -0300

    Desastre

--- src/laCopaDeLasCasas.pl
+++ src/laCopaDeLasCasas.pl
@@ -7,9 +7,23 @@ esDe(harry, gryffindor).
 esDe(draco, slytherin).
 esDe(luna, ravenclaw).
 
+casa(gryffindor).
+casa(slytherin).
+casa(ravenclaw).
+casa(hufflepuff).
+
 mago(Mago):-
     esDe(Mago, _).
+/*
+accion(fueraDeCama, mala).
+accion(bosque, mala).
+accion(tercerPiso, mala).
+accion(biblioteca, mala).
 
+accion(ajedrez, buena).
+accion(salvarAmigo, buena).
+accion(ganarleVoldemort, buena).
+*/
 bosque(50).
 biblioteca(10).
 tercerPiso(75).
@@ -19,10 +33,21 @@ ajedrez(50).
 salvarAmigo(50).
 ganarleVoldemort(60).
 
+
 accionBuena(ron, ajedrez).
 accionBuena(hermione, salvarAmigo).
 accionBuena(harry, ganarleVoldemort).
 %accionBuena(draco, ajedrez). #Test en accionRecurente
+
+/*acciones(harry, accion(fueraDeCama, 50)).
+acciones(harry, accion(bosque, 50)).
+acciones(harry, accion(tercerPiso, 75)).
+acciones(harry, accion(ganarleVoldemort, 50)).
+acciones(hermione, accion(salvarAmigo, 50)).
+*/
+acciones(Mago, accion(Puntos), Puntos).
+
+
 
 accionMala(harry, fueraDeCama).
 accionMala(harry, bosque).
@@ -44,3 +69,12 @@ accionRecurente(Accion):-
     mago(Mago), mago(OtroMago), Mago \= OtroMago,
     acciones(Mago, Accion),
     acciones(OtroMago, Accion).
+
+%puntajeCasa(Casa, PuntajeTotal):-
+%    casa(Casa).
+
+puntoEstudiante(Mago, Puntaje):-
+    mago(Mago),
+    findall(AccionesMalas, accionMala(Mago, AccionesMalas), ListaAcciones),
+    sum_list(ListaAcciones, Puntaje).
+    
