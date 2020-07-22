250dc43c5d7717ee06b008cdc93566b9213d955c
Juan Ignacio Yarbuh <jyarbuh@est.frba.utn.edu.ar>
2020-07-22 10:35:33 -0300

    Test

--- src/sombreroSeleccionador.pl
+++ src/sombreroSeleccionador.pl
@@ -1,3 +1,77 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parte 1 - Sombrero Seleccionador
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mago(harry, mestiza).
+mago(draco, puro).
+mago(hermione, impura).
+
+casa(gryffindor).
+casa(slytherin).
+casa(ravenclaw).
+casa(hufflepuff).
+
+odia(harry, slytherin).
+odia(draco, hufflepuff).
+
+corajudo(harry).
+
+amistoso(harry).
+
+orgulloso(harry).
+orgulloso(draco).
+orgulloso(hermione).
+
+inteligente(harry).
+inteligente(draco).
+inteligente(hermione).
+
+responsable(hermione).
+
+puedeEntrar(Mago, Casa):-
+    mago(Mago, _),
+    casa(Casa),
+    Casa \= slytherin.
+
+puedeEntrar(Mago, slytherin):-
+    mago(Mago, _),
+    not(mago(Mago, impura)).
+
+caracterApropiado(Mago, gryffindor):-
+    mago(Mago, _),
+    corajudo(Mago).
+caracterApropiado(Mago, slytherin):-
+    mago(Mago, _),
+    inteligente(Mago),
+    orgulloso(Mago).
+caracterApropiado(Mago, ravenclaw):-
+    mago(Mago, _),
+    inteligente(Mago),
+    responsable(Mago).
+caracterApropiado(Mago, hufflepuff):-
+    mago(Mago, _),
+    amistoso(Mago).
+
+puedeQuedar(Mago, Casa):-
+    mago(Mago, _),
+    casa(Casa),
+    puedeEntrar(Mago, Casa),
+    caracterApropiado(Mago, Casa),
+    not(odia(Mago, Casa)).
+puedeQuedar(hermione, gryffindor).
+
+cadenaDeAmistades([Mago]):-
+    amistoso(Mago).
+cadenaDeAmistades([Mago,OtroMago|Magos]):-
+    amistoso(Mago),
+    amistoso(OtroMago),
+    puedeEntrar(Mago, Casa),
+    puedeEntrar(OtroMago, Casa),
+    cadenaDeAmistades([OtroMago|Magos]).  
+
+%cadenaDeAmistades(ListaMagos):-
+%    forall()
+
+/*Definir un predicado ​cadenaDeAmistades/1​ que se cumple para una lista de magos si todos ellos secaracterizan por ser amistosos y cada uno podría estar en la misma casa 
+que el siguiente. ​No hace faltaque sea inversible​, se consultará de forma individual.
+*/
