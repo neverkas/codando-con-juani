b03328cd62e57b0013bf8dcebeb67fac735c798e
Juan Ignacio Yarbuh <jyarbuh@est.frba.utn.edu.ar>
2020-07-22 11:57:52 -0300

    Punto 1

--- src/laCopaDeLasCasas.pl
+++ src/laCopaDeLasCasas.pl
@@ -10,7 +10,37 @@ esDe(luna, ravenclaw).
 mago(Mago):-
     esDe(Mago, _).
 
-prohibido(lugar(bosque), 50).
-prohibido(lugar(biblioteca), 10).
-prohibido(lugar(tercerPiso), 75).
-prohibido(fueraDeCama, 50).
+bosque(50).
+biblioteca(10).
+tercerPiso(75).
+fueraDeCama(50).
+
+ajedrez(50).
+salvarAmigo(50).
+ganarleVoldemort(60).
+
+accionBuena(ron, ajedrez).
+accionBuena(hermione, salvarAmigo).
+accionBuena(harry, ganarleVoldemort).
+%accionBuena(draco, ajedrez). #Test en accionRecurente
+
+accionMala(harry, fueraDeCama).
+accionMala(harry, bosque).
+accionMala(harry, tercerPiso).
+accionMala(hermione, tercerPiso).
+accionMala(hermione, biblioteca).
+%accionMala(draco, bosque). #Test en accionRecurente
+
+acciones(Mago, Accion):-
+    accionBuena(Mago, Accion).
+acciones(Mago, Accion):-
+    accionMala(Mago, Accion).
+
+buenAlumno(Mago):-
+    mago(Mago),
+    not(accionMala(Mago, _)).
+
+accionRecurente(Accion):-
+    mago(Mago), mago(OtroMago), Mago \= OtroMago,
+    acciones(Mago, Accion),
+    acciones(OtroMago, Accion).
